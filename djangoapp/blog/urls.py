8fa9b956693dfc736bdec9170bb25ee031edf3cc
Danilo Ferreira <danilinhoferrer@gmail.com>
2023-05-21 20:57:34 -0300

    configurando o blog

---
+++ djangoapp/blog/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from blog.views import index
+
+
+app_name = 'blog'
+
+urlpatterns = [
+    path('', index, name='index'),
+]
+
--- scripts/commands.sh
+++ scripts/commands.sh
@@ -3,14 +3,8 @@
 # O shell irá encerrar a execução do script quando um comando falhar
 set -e
 
-while ! nc -z $POSTGRES_HOST $POSTGRES_PORT; do
-  echo "🟡 Waiting for Postgres Database Startup ($POSTGRES_HOST $POSTGRES_PORT) ..." 
-  sleep 2
-done
-
-echo "✅ Postgres Database Started Successfully ($POSTGRES_HOST:$POSTGRES_PORT)"
-
-python manage.py collectstatic  --noinput
-python manage.py makemigrations --noiunput
-python manage.py migrate --nopinput
-python manage.py runserver 0.0.0.0:8000
+wait_psql.sh
+collectstatic.sh
+makemigrations.sh
+migrate.sh
+runserver.sh
